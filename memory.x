6d7c9cf2863c329ea071bc0e9753ec9efd419dd5
Luis <luis_mtz907@live.com>
2025-06-14 21:17:19 -0500

    finally flashing

--- memory.x
+++ memory.x
@@ -1,15 +1,6 @@
 MEMORY {
-    BOOT2 : ORIGIN = 0x10000000, LENGTH = 0x100
-    FLASH : ORIGIN = 0x10000100, LENGTH = 2048K - 0x100
-    RAM   : ORIGIN = 0x20000000, LENGTH = 520K
+    FLASH : ORIGIN = 0x10000000, LENGTH = 2048K
+    RAM : ORIGIN = 0x20000000, LENGTH = 512K
 }
-
-SECTIONS {
-    /* ### Boot loader */
-    .boot2 ORIGIN(BOOT2) :
-    {
-        KEEP(*(.boot2));
-    } > BOOT2
-} INSERT BEFORE .text;
 
 _stack_start = ORIGIN(RAM) + LENGTH(RAM);
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,83 @@
+//! # GPIO 'Blinky' Example
+//!
+//! This application demonstrates how to control a GPIO pin on the RP2350.
+//! If there is an LED connected to GPIO 25 (as on the Pico 2), it will blink.
+
 #![no_std]
 #![no_main]
 
-use cortex_m_rt::entry;
+// Ensure we halt the program on panic
 use panic_halt as _;
 
-#[entry]
-fn main() -> !{
-    loop {}
+// Alias for our HAL crate
+use rp235x_hal as hal;
+
+// Some things we need
+use embedded_hal::delay::DelayNs;
+use embedded_hal::digital::OutputPin;
+
+/// Tell the Boot ROM about our application
+#[link_section = ".start_block"]
+#[used]
+pub static IMAGE_DEF: hal::block::ImageDef = hal::block::ImageDef::secure_exe();
+
+/// External high-speed crystal on the Raspberry Pi Pico 2 board is 12 MHz
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
+/// Entry point to our bare-metal application.
+#[hal::entry]
+fn main() -> ! {
+    // Grab our singleton objects
+    let mut pac = hal::pac::Peripherals::take().unwrap();
+
+    // Set up the watchdog driver - needed by the clock setup code
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+
+    // Configure the clocks
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .unwrap();
+
+    let mut timer = hal::Timer::new_timer0(pac.TIMER0, &mut pac.RESETS, &clocks);
+
+    // The single-cycle I/O block controls our GPIO pins
+    let sio = hal::Sio::new(pac.SIO);
+
+    // Set the pins to their default state
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    // Configure GPIO25 as an output
+    let mut led_pin = pins.gpio25.into_push_pull_output();
+    
+    loop {
+        led_pin.set_high().unwrap();
+        timer.delay_ms(500);
+        led_pin.set_low().unwrap();
+        timer.delay_ms(500);
+    }
 }
+
+/// Program metadata for `picotool info`
+#[link_section = ".bi_entries"]
+#[used]
+pub static PICOTOOL_ENTRIES: [hal::binary_info::EntryAddr; 5] = [
+    hal::binary_info::rp_cargo_bin_name!(),
+    hal::binary_info::rp_cargo_version!(),
+    hal::binary_info::rp_program_description!(c"Blinky Example"),
+    hal::binary_info::rp_cargo_homepage_url!(),
+    hal::binary_info::rp_program_build_attribute!(),
+];
+
+// End of file
