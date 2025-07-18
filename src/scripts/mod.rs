5a3112471a132deafbc3863ff585058e76e27e52
lmx154 <luis_mtz907@live.com>
2025-07-18 16:51:16 -0500

    remove old files

--- src/scripts/mod.rs
+++
@@ -1,5 +0,0 @@
-//! Scripts module
-//! 
-//! Contains utility scripts for testing and debugging hardware.
-
-pub mod i2c_scanner;
--- src/sensors/mod.rs
+++
@@ -1,8 +0,0 @@
-//! # Sensors Module
-//!
-//! This module contains all sensor-related functionality and drivers.
-
-pub mod gps;
-pub mod pcf8563;
-pub mod bmp388;
-pub mod sensor_manager;
